aa75eaa0616b2fb6c3d1dc8775f0926651acb0d6
Tulioleal <tuliolealdev@gmail.com>
2025-01-17 16:24:50 -0300

    add country routes setup in index file

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { setCountriesRoutes } = require("./routes/countries");
+const { setCountriesRoutes, setCountryRoutes } = require("./routes");
 const {
   PORT
 } = require("./const");
@@ -8,6 +8,7 @@ const app = express();
 app.use(express.json());
 
 setCountriesRoutes(app);
+setCountryRoutes(app);
 
 app.listen(PORT, () => {
   console.log("Server Listening on PORT:", PORT);
