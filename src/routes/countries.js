6b40ce202829caf7ec62ef5b90d7763b04b1d153
Tulioleal <tuliolealdev@gmail.com>
2025-01-17 17:57:10 -0300

    add CORS support to routes for handling cross-origin requests

--- src/routes/countries.js
+++ src/routes/countries.js
@@ -1,9 +1,13 @@
+const cors = require('cors');
 const { CountriesController } = require('../controllers/countries.controller');
 
 function setCountriesRoutes(app) {
   const countriesController = new CountriesController();
 
-  app.get('/countries', (request, response) =>
+  // Allow all origins (or specify allowed origins)
+  app.use(cors());
+
+  app.get('/countries', cors(), (request, response) =>
     countriesController.getCountries(request, response));
 }
 
--- src/routes/country.js
+++ src/routes/country.js
@@ -1,9 +1,13 @@
+const cors = require('cors');
 const { CountryController } = require('../controllers/country.controller');
 
 function setCountryRoutes(app) {
   const countryController = new CountryController();
 
-  app.get('/countries/:countryCode', (request, response) =>
+  // Allow all origins (or specify allowed origins)
+  app.use(cors());
+
+  app.post('/countries', (request, response) =>
     countryController.getCountry(request, response));
 }
 
